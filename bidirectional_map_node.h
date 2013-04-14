78dc6e8200947cc24646e8bae368a72b4ea391cc
Tor E Hagemann <hagemt@rpi.edu>
2013-04-14 18:49:26 -0400

    Add prev and next functions to nodes

--- bidirectional_map_node.h
+++ bidirectional_map_node.h
@@ -10,10 +10,74 @@
 template <typename A, typename B>
 struct Node {
 
-	/* Representation is data, linked node, and children */
+	/* Representation captures a piece of data */
 	A *data;
+	/* Each Node is reflexively linked */
 	Node<B, A> *link;
-	Node<A, B> *left, *right;
+	/* And knows its children and parent */
+	const Node<A, B> *left, *right, *parent;
+
+//protected: // Utilities
+
+	Node<A, B> *prev() const {
+		const Node<A, B> *n = this;
+		/* Start from here, check upward for left children */
+		while (n->left == nullptr) {
+			/* If there is no parent, this node is minimal */
+			if (n->parent == nullptr) {
+				/* No smaller node exists */
+				return nullptr;
+			}
+			n = n->parent;
+		}
+		/* To the left is a valid node, get the right-most child */
+		n = n->left;
+		while (n->right) {
+			n = n->right;
+		}
+		return const_cast<Node<A, B> *>(n);
+	}
+
+	Node<A, B> *next() const {
+		const Node<A, B> *n = this;
+		/* Start from here, check upward for right children */
+		while (n->right == nullptr) {
+			/* If there is no parent, this node is maximal */
+			if (n->parent == nullptr) {
+				/* No larger node exists */
+				return nullptr;
+			}
+			n = n->parent;
+		}
+		/* To the right is a valid node, get the left-most child */
+		n = n->right;
+		while (n->left) {
+			n = n->left;
+		}
+		return const_cast<Node<A, B> *>(n);
+	}
+
+private:
+
+	/* FIXME shouldn't do this? */
+	void destroy() {
+		if (data) { delete data; }
+		if (link) { link->link = nullptr; }
+	}
+
+	/* Copying duplicates data, shallow-copies link, etc. */
+	void copy(const Node<A, B> &n) {
+		/* Replace the data */
+		this->destroy();
+		data = new A(*n);
+		/* Update all the pointers */
+		link = n.link;
+		left = n.left;
+		right = n.right;
+		parent = n.parent;
+	}
+
+public:
 
 	/* Constructors/destructors */
 	explicit Node(const A &a = A()) :
