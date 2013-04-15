fcf9d58bd5c7d20e53ea10f4b1956ed2d96afb83
Tor E Hagemann <hagemt@rpi.edu>
2013-04-14 20:37:48 -0400

    Simplify the map test case

--- map.cpp
+++ map.cpp
@@ -1,25 +1,27 @@
 #include <cassert>
 #include <iostream>
 #include <string>
-#include <tuple>
 #include <utility>
 
 #include "bidirectional_map.h"
 
-typedef bidirectional_map<int, std::string> map;
-
 #define VALUE "zero"
+
+typedef bidirectional_map<int, std::string> MAP;
 
 int main(void) {
 	/* Place something in the map */
-	map m;
-	m.insert(std::make_pair(0, VALUE));
+	MAP m;
+	//m.insert(std::make_pair(0, VALUE));
 	/* Check that find works */
-	map::key_iterator it1, it2, itend = m.key_end();
-	std::tie(it1, it2) = std::make_tuple(m.find(0), m.find(1));
+	MAP::key_iterator it1, it2, itend = m.key_end();
+	it1 = m.find(0);
+	it2 = m.find(1);
 	assert(it1 != itend);
 	assert(it2 == itend);
 	/* Operator test */
-	std::cout << m[0] << std::endl;
+	using std::cout;
+	using std::endl;
+	cout << m[0] << endl;
 	return 0;
 }
